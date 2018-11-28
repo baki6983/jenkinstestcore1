81d8c34bb0f2a808674bd2f5366b308189a54c1d
balakrishna Suggu <balakrishnasuggu@outlook.com>
2018-11-28 22:28:27 +0100

    test5

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace jenkinscore1Demo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "Baki", "Hello1" };
+            return new string[] { "Baki", "Helflo1" };
         }
 
         // GET api/values/5
